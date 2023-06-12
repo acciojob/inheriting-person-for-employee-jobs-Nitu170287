08a6cef1f8724f682b1bd4910ffec8c3a88d4afc
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-12 15:56:43 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 // complete this js code
 function Person(name, age) {
-	greet(){
-		console.log("Hello, my name is "+this.name+" ,I am "+this.age+" years old.")
-	}
+    this.name = name;
+    this.age = age;
 }
+Person.prototype.greet =  function(){
+   console.log("Hello, my name is "+this.name+" and I am "+this.age+" years old.")
+};
 
 function Employee(name, age, jobTitle) {
-	const info = new Person(name, age)
-	jobGreet(){
-		console.log("Hello, my name is "+info.name+" ,I am "+info.age+" years old, and my job title is "+this.jobTitle)
-	}
+    Person.call(this,name,age);
+    this.jobTitle = jobTitle;
 }
 
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
+  };
+
+var person = new Person("John", 30);
+person.greet();
+
+var employee = new Employee("Jane", 25, "Manager");
+employee.greet()
+employee.jobGreet()
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
